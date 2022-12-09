bd6dcc56dac04e7f568c3f565263f0f88214a2e8
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-09 12:27:02 +0000

    Implemented pinia plugin

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import { createPinia, PiniaVuePlugin } from 'pinia'
 
 Vue.config.productionTip = false
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
 
 new Vue({
   router,
   vuetify,
+  pinia,
   render: h => h(App)
 }).$mount('#app')
