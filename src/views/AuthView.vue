8847b40f2eb476cc4e39f87ad7ff8b9c0eef4d07
Esteban <esteban.c.ojedahdez@gmail.com>
2022-12-09 17:43:30 +0000

    ADDED: Authview created

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,13 +1,15 @@
 <template>
-  <div></div>
+  <div>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView',
 
-  components: {
-    //
-  }
 }
 </script>
+
+<style scoped>
+
+</style>
