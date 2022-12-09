83c959590834725ab21b8c1e439b621e6b907200
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-09 12:27:26 +0000

    ADDED authStore for token & email

---
+++ src/stores/store.js
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+import { useStorage } from '@vueuse/core'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => {
+    return {
+      token: useStorage('token', null),
+      email: useStorage('email', null)
+    }
+  },
+  getters: {
+    isLoggedIn () {
+      return this.token != null
+    },
+    userEmail () {
+      return this.email
+    }
+  },
+  actions: {
+    logout () {
+      this.token = null
+      this.email = null
+    },
+    login (token, email) {
+      this.token = token
+      this.email = email
+    }
+  }
+})
