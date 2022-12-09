90be88078fa6ae6aac52b8fc639507bcf2f3e4c4
Alejandro <alejandrocs402@gmail.com>
2022-12-09 10:58:19 +0000

    navbar iniciation

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,20 @@
 <template>
   <v-app>
-    <!-- Barra de tareas -->
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <!-- Logo de la barra de tareas -->
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        class="mx-2"
-        elevation="2"
-        rounded
-      >
-      LOGIN
-      </v-btn>
-      <v-btn
-        class="mx-2"
-        elevation="2"
-        rounded
-      >
-      SIGNUP
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
       <!-- Vista del View -->
+      <navBar />
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import navBar from './components/NavBar.vue'
 export default {
   name: 'App',
-
+  components: {
+    navBar
+  },
   data: () => ({
     //
   })
---
+++ src/components/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <v-app-bar app color="#DD7225">
+      <v-app-bar-nav-icon color="white" @click="drawer = true"></v-app-bar-nav-icon>
+      <div class="d-flex align-center">
+        <!-- Logo de la barra de tareas -->
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+      <v-spacer />
+      <v-toolbar-title class="title" >Rinku</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer fixed v-model="drawer" temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+color: white;
+}
+</style>
