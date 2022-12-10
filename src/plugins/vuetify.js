84aadcab45c0dd256f5caa816247251a6ae2a74e
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-10 20:15:34 +0000

    MODIFIED: Custom colors for components

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#DD7225',
+        anchor: '#6C311B'
+      },
+      dark: {}
+    }
+  }
 })
