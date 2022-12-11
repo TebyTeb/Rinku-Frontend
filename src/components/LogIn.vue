4d15e37640f4f44679e1385c42f544baee3f7f61
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-11 13:41:31 +0000

    Added login functionality

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -5,12 +5,16 @@
           outlined
           prepend-inner-icon="mdi-email"
           label="Email"
+          v-model="loginData.email"
         ></v-text-field>
         <v-text-field
+          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="show ? 'text' : 'password'"
           outlined
           prepend-inner-icon="mdi-lock"
           label="Password"
-          id="password"
+          v-model="loginData.password"
+          @click:append="show = !show"
         ></v-text-field>
       </v-form>
       <v-card-actions
@@ -20,18 +24,41 @@
       color="primary"
       class="px-6 mb-5"
       rounded
+      @click.prevent="login"
       >Login
     </v-btn>
     <a
       @click="toggleForm"
-    >Don't have an account? - <span style="color: #DD7225;">Sign Up</span></a>
+    >Don't have an account? <span style="color: #DD7225;">Sign Up</span></a>
       </v-card-actions>
     </v-card-text>
 </template>
 
 <script>
+import API from '../services/api.js'
+import { useAuthStore } from '../stores/store'
 export default {
+  data () {
+    return {
+      show: false,
+      loginData: {
+        email: '',
+        password: ''
+      }
+    }
+  },
   methods: {
+    async login () {
+      const response = await API.login(this.loginData)
+      if (response.error) {
+        alert('wrong username/password') // No funciona
+      } else {
+        useAuthStore().login(response.token, response.email)
+        this.loginData.email = ''
+        this.loginData.password = ''
+        this.$router.push({ name: 'subscription' })
+      }
+    },
     toggleForm () {
       this.$emit('toggleForm')
     }
