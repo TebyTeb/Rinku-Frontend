5b0a9a83205e9e17342e2571143349f01f6ab857
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-11 13:42:22 +0000

    created api service with login function

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const API = axios.create({
+  baseURL: 'http://localhost:3000/api'
+})
+
+async function login (loginData) {
+  try {
+    const { data } = await API.post('/auth/login', loginData)
+    return data
+  } catch (error) {
+    return { error: error.message }
+  }
+}
+
+export default {
+  login
+}
