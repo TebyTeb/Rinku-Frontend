a25aa19e83a44ad46c411c93b52972d04475dc50
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-11 14:12:00 +0000

    Added fade transition between login & signup

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -15,8 +15,13 @@
       <p>Create your account</p>
     </div>
     <v-container class="logger">
-      <LogIn v-if="isLogin" @toggleForm="toggleForm" />
-      <SignUp v-else @toggleForm="toggleForm" />
+      <transition
+        mode="out-in"
+        name="fade"
+      >
+        <LogIn v-if="isLogin" @toggleForm="toggleForm" />
+        <SignUp v-else @toggleForm="toggleForm" />
+      </transition>
     </v-container>
   </div>
 </template>
@@ -54,8 +59,13 @@ export default {
   border-radius: 0 0 5rem 0;
   box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.59);
 }
-
 .logger {
   padding-top: 100px;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
