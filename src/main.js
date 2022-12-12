15f1ef754502e97b0a0c9a041b11a824cbdec56e
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-12 17:19:34 +0000

    Pinia Fixes

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import { createPinia, PiniaVuePlugin } from 'pinia'
+import { createPinia } from 'pinia'
 
-Vue.use(PiniaVuePlugin)
 const pinia = createPinia()
+
+Vue.use(pinia)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
-  pinia,
   render: h => h(App)
 }).$mount('#app')
