54657b46ea51c1ea2eb9f5fdbda85c5e4ed18b79
Alejandro <alejandrocs402@gmail.com>
2022-12-13 11:51:59 +0000

    navbar edit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -57,6 +57,17 @@
               <v-icon>mdi-window-close</v-icon>
             </v-list-item-icon>
           </v-list-item>
+          <v-list-item>
+            <v-spacer />
+            <span>{{store.email}}</span>
+            <v-avatar size="40" class="avatar">
+              <img
+                alt="user"
+                src="https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg"
+              >
+            </v-avatar>
+          </v-list-item>
+          <v-divider></v-divider>
           <v-list-item v-for="(link,keys) in loggedLinks" :key="keys" class="item-hover">
             <RouterLink :to=link.name class="links">
               <v-list-item-icon>
@@ -64,6 +75,12 @@
               </v-list-item-icon>
               <v-list-item-title>{{link.title}}</v-list-item-title>
             </RouterLink>
+          </v-list-item>
+          <v-list-item class="item-hover" @click="logoutuser">
+              <v-list-item-icon>
+                <v-icon >mdi-logout</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title class="stylecolor">Logout</v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -114,6 +131,11 @@ export default {
         title: 'Home'
       },
       {
+        name: 'subscription',
+        icon: 'mdi-youtube-subscription',
+        title: 'Subscription'
+      },
+      {
         name: 'about',
         icon: 'mdi-account-group',
         title: 'About us'
@@ -122,15 +144,18 @@ export default {
         name: 'FAQ',
         icon: 'mdi-help-circle',
         title: 'FAQ'
-      },
-      {
-        name: 'logout',
-        icon: 'mdi-help-circle',
-        title: 'LOGOUT'
       }
     ]
   }),
-  components: { RouterLink }
+  components: { RouterLink },
+  methods: {
+    logoutuser () {
+      this.store.logout()
+      if (this.$route.path !== '/') {
+        this.$router.push('/')
+      }
+    }
+  }
 }
 </script>
 
@@ -143,5 +168,11 @@ export default {
 .links{
   text-decoration: none;
   display: flex;
+}
+.avatar{
+  margin-left: 10px;
+}
+.stylecolor{
+  color: #6c311b;
 }
 </style>
