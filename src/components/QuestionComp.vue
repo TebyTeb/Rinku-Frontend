cf9524f563ec0c37f0fc143f5fee320c3f421962
Alejandro <alejandrocs402@gmail.com>
2022-12-15 13:18:00 +0000

    solve bug FAQ

--- src/components/QuestionComp.vue
+++ src/components/QuestionComp.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <v-card class="mx-auto" >
+    <v-card class="mx-auto cardMar" elevation="6">
       <v-card-actions @click="show = !show" class="fer">
         <span class="exp">{{ data.tittle }}</span>
+        <v-spacer></v-spacer>
         <v-btn icon >
           <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
         </v-btn>
       </v-card-actions>
 
       <v-expand-transition>
-        <div v-show="show">
+        <div v-show="show" >
+          <v-divider></v-divider>
           <v-card-text>
             I'm a thing. But, like most politicians, he promised more than he
             could deliver. You won't have time for sleeping, soldier, not with
@@ -37,9 +39,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.cardMar{
+  margin-bottom: 15px;
+}
 .exp {
  color: #dd7225;
- text-align: center;
 }
 .fer {
   display: flex;
