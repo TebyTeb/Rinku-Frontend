822ac2a5e01fe9b921f2778fc633bbd3e2422fe9
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-15 14:26:57 +0000

    Hero Restyling done

--- src/components/HeroComp.vue
+++ src/components/HeroComp.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="bar">
-    <div class="foo">
-      <span class="hero"
-        >Bienvenido/a a nuestro servicio de gestión de subcripciones</span
-      >
+  <div class="wrapper">
+    <div class="container">
+      <h1 class="hero">
+        Your subscriptions, all in one place
+      </h1>
+      <h3 class="subtitle">
+        Rinku is a web service that allows you to gather all your subscriptions info together. No more undesired charges due to forgetting to cancel in time! Let Rinku remind you about them!
+      </h3>
       <div class="logandsign">
-        <RouterLink to="signup"><v-btn color="warning" dark> Create an account </v-btn></RouterLink>
+          <v-btn
+            :to="{name: 'signup'}"
+            rounded
+            class="px-10 py-5"
+            color="primary"
+            dark
+          > Get Started </v-btn>
         <RouterLink to="login" class="sign">Already a member?</RouterLink>
 
       </div>
@@ -22,36 +31,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.foo {
-  background-color: rgba(0, 0, 0, 0.7);
-  height: 400px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-.bar {
+.wrapper {
   background-image: url(../assets/homeImage.jpg);
   background-size: cover;
 }
+.container {
+  // background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(255, 255, 255, 1));
+  background: linear-gradient(0deg, rgba(255,255,255,) 10%, rgba(0,0,0,0.7) 65%);
+  height: 33rem;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+}
 .hero {
-  font-size: 40px;
   color: white;
-  text-align: center;
-  margin: 0 20px 0 20px;
+  margin: 0 1.5rem;
+  margin-top: 1.5rem;
+}
+.subtitle {
+  color: white;
+  margin: 0 1.5rem;
+  margin-top: 0.5rem;
+  font-weight: lighter;
+  font-size: medium;
 }
 .logandsign {
-  margin-top: 2rem;
+  margin-top: 6rem;
   display: flex;
   flex-direction: column;
   align-items: center;
 }
 .sign {
+  margin-top: 1rem;
   text-decoration: none;
-  color: white;
-}
-a {
-  text-decoration: none;
+  font-weight: bold;
 }
 </style>
