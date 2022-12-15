97ccd24690b229e720c51f988810e0e5e04a7a8a
Alejandro <alejandrocs402@gmail.com>
2022-12-15 17:04:42 +0000

    edit text

--- src/components/QuestionComp.vue
+++ src/components/QuestionComp.vue
@@ -13,11 +13,7 @@
         <div v-show="show" >
           <v-divider></v-divider>
           <v-card-text>
-            I'm a thing. But, like most politicians, he promised more than he
-            could deliver. You won't have time for sleeping, soldier, not with
-            all the bed making you'll be doing. Then we'll go with that data
-            file! Hey, you add a one and two zeros to that or we walk! You're
-            going to do his laundry? I've got to find a way to escape.
+            {{ data.text }}
           </v-card-text>
         </div>
       </v-expand-transition>
